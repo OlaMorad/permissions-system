12cf67eedeec4941455712111cf75ac543517dcb
Ola Morad <152977933+OlaMorad@users.noreply.github.com>
2025-06-04 18:41:54 +0300

    reset password

--- app/Http/Controllers/managerController.php
+++ app/Http/Controllers/managerController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 
+use App\Services\ManagerService;
 use App\Services\RegisterService;
+use Illuminate\Support\Facades\DB;
 use Spatie\Permission\Models\Role;
 use App\Http\Resources\successResource;
 use App\Http\Requests\RegisterManagerRequest;
@@ -11,12 +13,12 @@ use App\Http\Requests\RegisterManagerRequest;
 class ManagerController extends Controller
 {
 
-    public function __construct(protected RegisterService $managerService) {}
+    public function __construct(protected RegisterService $registerService ,protected ManagerService $managerService ) {}
 
 
     public function create_manager(RegisterManagerRequest $request, $role_id)
     {
-        $this->managerService->registerManager($request->validated());
+        $this->registerService->registerManager($request->validated());
 
         return new successResource([]);
     }
@@ -28,5 +30,18 @@ class ManagerController extends Controller
             'roles' => $roles
         ]);
 
+    }
+
+
+    public function show_my_employees(){
+
+ return $response=  $this->managerService->show_my_employees();
+
+
+    }
+
+    public function show_all_managers(){
+        $managers_id=dB::table("managers")->pluck('user_id');
+        return DB::table('users')->whereIn('id',$managers_id)->select('name','email')->get();
     }
 }
