0201e82e795562475a76c3d82859985e9865cb45
stringAsmaa <152689060+stringAsmaa@users.noreply.github.com>
2025-06-17 23:03:12 +0300

    add show internal mail details

--- app/Http/Controllers/InternalMailController.php
+++ app/Http/Controllers/InternalMailController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\IdInternalMailRequest;
 use App\Http\Requests\InternalMailRequest;
 use App\Http\Requests\StatusRequest;
 use App\Http\Resources\successResource;
@@ -15,26 +16,42 @@ class InternalMailController extends Controller
 
     public function create_internal_mail(InternalMailRequest $request){
 
+        //انشاء البريد الداخلي من قب الموظف او الادمن او السب ادمن
       $response=  $this->InternalMailService->create_internal_mail($request);
       return new successResource([]);
     }
 
 
-
+//عرض البريد الصادر حسب  الحالة
     public function show_internal_mails_by_status($status){
         return  $response=  $this->InternalMailService->show_internal_mails_by_status($status);
        return new successResource([$response]);
     }
 
+//تعديل حالة البريد من قبل المدير
     public function edit_status_internal_mails(StatusRequest $request){
 
    return $response=  $this->InternalMailService->edit_status_internal_mails($request);
 }
-//عرض البريد الداخلي الوارد للمدير
+
+
+//عرض البريد الداخلي الوارد للمديراو الادمن او السب ادمن
 public function show_import_internal_mails(){
      return $response=  $this->InternalMailService->show_import_internal_mails();
 }
 
+//عرض تفاصيل البريد الصادر
+public function show_export_internal_mail_details(IdInternalMailRequest $id){
+
+return $this->InternalMailService->show_export_internal_mail_details($id);
+
+}
+
+public function show_import_internal_mail_details(IdInternalMailRequest $id){
+
+return $this->InternalMailService->show_import_internal_mail_details($id);
+
+}
 
 
 }
--- routes/api.php
+++ routes/api.php
@@ -63,10 +63,13 @@ Route::middleware(['throttle:10,1'])->group(
             Route::get('show_internal_mails_by_status/{status}', 'show_internal_mails_by_status')->middleware('Verify.Session');
             ROute::post('edit_status_internal_mails', 'edit_status_internal_mails')->middleware('Verify.Session');
             Route::get('show_import_internal_mails', 'show_import_internal_mails')->middleware('Verify.Session');
+            Route::get('show_export_internal_mail_details', 'show_export_internal_mail_details')->middleware('Verify.Session');
+            Route::get('show_import_internal_mail_details','show_import_internal_mail_details')->middleware('Verify.Session');
         });
 
-        Route::controller(FormController::class)->group(function () {
-                    Route::prefix('form')->group(function () { //  تحميل نموذج من ملف Word
+        Route::controller(FormController::class)->group(
+            function () {
+                Route::prefix('form')->group(function () { //  تحميل نموذج من ملف Word
                     Route::post('/upload-word', 'storeFromWord'); //  إنشاء نموذج يدوي
                     Route::post('/manual', 'storeManually');
                     Route::get('/show_all', 'index')->middleware('auth:api', 'role:Head of Front Desk');
