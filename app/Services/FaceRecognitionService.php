db5bf67290b4ef8cc1f9616f032539f020b6c930
stringAsmaa <152689060+stringAsmaa@users.noreply.github.com>
2025-08-06 03:28:35 +0300

    merge

--- app/Services/FaceRecognitionService.php
+++ app/Services/FaceRecognitionService.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Services;
+
 use Illuminate\Support\Facades\Http;
 use App\Http\Resources\successResource;
 
@@ -8,26 +9,15 @@ class FaceRecognitionService
 {
     public function compareFaces($path1, $path2)
     {
-<<<<<<< Updated upstream
-$response = Http::asMultipart()
-    ->attach('img1', base64_decode($base64Image1), 'img1.jpg')
-    ->attach('img2', base64_decode($base64Image2), 'img2.jpg')
-    ->post('http://127.0.0.1:5000/process');
-
-
-=======
         $response = Http::asMultipart()
             ->attach('img1', file_get_contents($path1), 'img1.jpg')
             ->attach('img2', file_get_contents($path2), 'img2.jpg')
             ->post('http://127.0.0.1:5000/process');
->>>>>>> Stashed changes
 
         if ($response->successful()) {
 
-                 $data = $response->json();
-
-    return new successResource($data);
-          
+            $data = $response->json();
+            return new successResource($data);
         }
 
         throw new \Exception('Face API Request failed: ' . $response->body());
