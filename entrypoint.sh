ad8b48c15a7638b4cdc0bba49d3102d3c968db6c
stringAsmaa <152689060+stringAsmaa@users.noreply.github.com>
2025-09-06 23:19:42 +0300

    f

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,17 +8,28 @@ while ! mysqladmin ping -h "$DB_HOST" -P "$DB_PORT" --silent; do
     sleep 3
 done
 
-echo "MySQL is ready! Running migrations..."
+echo "MySQL is ready! Running migrations and clearing cache..."
+
+# تعطيل foreign key مؤقتًا
 mysql -h "$DB_HOST" -P "$DB_PORT" -u "$DB_USERNAME" -p"$DB_PASSWORD" -e "SET FOREIGN_KEY_CHECKS=0;"
 
-php artisan db:wipe --force
-# php artisan migrate --force
+# تنفيذ المايغريشن
+php artisan migrate --force
+
+# تنظيف الكاش (config, route, view)
+php artisan config:clear
+php artisan cache:clear
+php artisan route:clear
+php artisan view:clear
+
+# تفعيل foreign key بعد المايغريشن
 mysql -h "$DB_HOST" -P "$DB_PORT" -u "$DB_USERNAME" -p"$DB_PASSWORD" -e "SET FOREIGN_KEY_CHECKS=1;"
 
 echo "Seeding database..."
-# php artisan db:seed --force
+php artisan db:seed --force
 
 echo "Starting Laravel server..."
 echo "PORT is $PORT"
 
+# تشغيل السيرفر على البورت المحدد من Railway
 exec php -S 0.0.0.0:$PORT -t public
