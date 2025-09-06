ac6ec6f2c701f21698365127b08c1ded3d86b05c
Ola Morad <152977933+OlaMorad@users.noreply.github.com>
2025-09-06 14:28:11 +0300

    dockerfile

--- database/migrations/2025_07_15_225642_create_exam_requests_table.php
+++ database/migrations/2025_07_15_225642_create_exam_requests_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
         Schema::create('exam_requests', function (Blueprint $table) {
             $table->id();
             $table->uuid('uuid')->unique();
-            $table->foreignId('doctor_id')->references('id')->on('Doctors');
+            $table->foreignId('doctor_id')->references('id')->on('doctors');
              $table->foreignId('form_content_id')->references('id')->on('form_contents');
             $table->string('status')->default(ExamRequestEnum::PENDING->value);
             $table->timestamps();
