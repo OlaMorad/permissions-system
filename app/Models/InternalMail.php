1a41bae5832d379ac00aba90a900b9376ff8ef41
stringAsmaa <152689060+stringAsmaa@users.noreply.github.com>
2025-09-07 15:16:14 +0300

    f

---
+++ app/Models/InternalMail.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models;
+
+use App\Enums\StatusInternalMail;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+class InternalMail extends Model
+{
+    protected $fillable=[
+        'from_user_id',
+        'status',
+        'subject',
+        'body',
+        'uuid',
+    ];
+
+       protected $casts = [
+        'status' => StatusInternalMail::class,
+    ];
+
+    public function fromUser()
+{
+    return $this->belongsTo(User::class, 'from_user_id');
+}
+
+
+
+
+    public function paths()
+    {
+        return $this->belongsToMany(Path::class, 'internal_mail_paths');
+    }
+
+      protected static function booted()
+    {
+        static::creating(function ($transaction) {
+            $transaction->uuid = (string) Str::uuid();
+        });
+    }
+}
