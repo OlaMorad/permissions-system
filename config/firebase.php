64ffc22aaca376dd5c03f1c44c4c589f3fb335be
stringAsmaa <152689060+stringAsmaa@users.noreply.github.com>
2025-09-08 10:52:00 +0300

    fix InternalMail class and clear caches

--- config/firebase.php
+++ config/firebase.php
@@ -51,7 +51,7 @@ return [
              */
 
             'credentials' => [
-                'credentials_file' =>  json_decode(env('FIREBASE_CREDENTIALS'), true),
+                'credentials_file' => storage_path('app/' . env('FIREBASE_CREDENTIALS', '')),
                 'project_id' => env('FIREBASE_PROJECT_ID', null),
                 'sender_id' => env('FIREBASE_SENDER_ID', null),
 
